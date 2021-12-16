e3e1746d38a5db0f1e1668c3e0472498dc8ecaf6
James Coleman <mrjameslcoleman@gmail.com>
2021-12-16 14:44:41 +0000

    Completed Day 14 Part 2

--- Advent of Code 2021/Day 14 - Extended Polymerization.playground/Pages/Algorithm 2 Counting Letters.xcplaygroundpage/Contents.swift
+++ Advent of Code 2021/Day 14 - Extended Polymerization.playground/Pages/Algorithm 2 Counting Letters.xcplaygroundpage/Contents.swift
@@ -172,22 +172,133 @@ func generation(_ generation: Int, from input: Counts, rules: Rules) -> Counts {
     return newPairs
 }
 
-/*
+func answer(from input: CharacterCount) -> Int? {
+    let sorted = input.sorted(by: { $0.value > $1.value })
+    
+    guard let biggest = sorted.first, let smallest = sorted.last else { return nil }
+    
+    return biggest.value - smallest.value
+}
+
+
 let exampleGeneration10 = generation(10, from: exampleTemplatePairs, rules: examplePolymerRules)
 
-let exampleGen10Sorted = exampleGeneration10.characters.sorted(by: { $0.value > $1.value })
-
-if let biggest = exampleGen10Sorted.first, let smallest = exampleGen10Sorted.last {
-    biggest.value - smallest.value // 1588 (correct)
-}
+answer(from: exampleGeneration10.characters) // 1588 (correct)
 
 let exampleGeneration40 = generation(40, from: exampleTemplatePairs, rules: examplePolymerRules)
 
-let exampleGen40Sorted = exampleGeneration40.characters.sorted(by: { $0.value > $1.value })
+answer(from: exampleGeneration40.characters) // 2188189693529 (correct)
 
-if let biggest = exampleGen40Sorted.first, let smallest = exampleGen40Sorted.last {
-    biggest.value - smallest.value // 2188189693529 (correct)
-}
- */
+let puzzlePairs = polymerPairs(from: "PFVKOBSHPSPOOOCOOHBP")
+let puzzleRulesInput = """
+    FV -> C
+    CP -> K
+    FS -> K
+    VF -> N
+    HN -> F
+    FF -> N
+    SS -> K
+    VS -> V
+    BV -> F
+    HC -> K
+    BP -> F
+    OV -> N
+    BF -> V
+    VH -> V
+    PF -> N
+    FC -> S
+    CS -> B
+    FK -> N
+    VK -> H
+    FN -> P
+    SH -> V
+    CV -> K
+    HP -> K
+    HO -> C
+    NO -> V
+    CK -> C
+    VB -> S
+    OC -> N
+    NS -> C
+    NF -> H
+    SF -> N
+    NK -> S
+    NP -> P
+    OO -> S
+    NH -> C
+    BC -> H
+    KS -> H
+    PV -> O
+    KO -> K
+    OK -> H
+    OH -> H
+    BH -> F
+    NB -> B
+    FH -> N
+    HV -> F
+    BN -> S
+    ON -> V
+    CB -> V
+    CF -> H
+    FB -> S
+    KF -> S
+    PS -> P
+    OB -> C
+    NN -> K
+    KV -> C
+    BK -> H
+    SN -> S
+    NC -> H
+    PK -> B
+    PC -> H
+    KN -> S
+    VO -> V
+    FO -> K
+    CH -> B
+    PH -> N
+    SO -> C
+    KH -> S
+    HB -> V
+    HH -> B
+    BB -> H
+    SC -> V
+    HS -> K
+    SP -> V
+    KB -> N
+    VN -> H
+    HK -> H
+    KP -> K
+    OP -> F
+    CO -> B
+    VP -> H
+    OS -> N
+    OF -> H
+    KK -> N
+    CC -> K
+    BS -> C
+    VV -> O
+    CN -> H
+    PB -> P
+    BO -> N
+    SB -> H
+    FP -> F
+    SK -> F
+    PO -> S
+    KC -> H
+    VC -> H
+    NV -> N
+    HF -> B
+    PN -> F
+    SV -> K
+    PP -> K
+    """
 
+let puzzleRules = polymerRules(from: puzzleRulesInput)
 
+let puzzleGen10 = generation(10, from: puzzlePairs, rules: puzzleRules)
+
+answer(from: puzzleGen10.characters) // 2937 (correct)
+
+let puzzleGen40 = generation(40, from: puzzlePairs, rules: puzzleRules)
+
+answer(from: puzzleGen40.characters) // 3390034818249 (correct)
