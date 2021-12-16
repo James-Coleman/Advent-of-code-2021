f5c45c40f588f48cf9f9654aced2d88e2bf9a291
James Coleman <mrjameslcoleman@gmail.com>
2021-12-16 17:35:22 +0000

    Got something recursive(?) working for Day 16 but seems to be infinitely looping.

--- Advent of Code 2021/Day 16 - Packet Decoder.playground/Contents.swift
+++ Advent of Code 2021/Day 16 - Packet Decoder.playground/Contents.swift
@@ -50,10 +50,55 @@ enum PacketError: Error {
     case stringIsNotValidHexadecimal(String)
 }
 
-struct LiteralPacket {
+enum Packet {
+    case literal(LiteralPacket)
+    case `operator`(OperatorPacket)
     
+    init(string: String) throws {
+        guard let int = Int(string, radix: 16) else { throw PacketError.stringIsNotValidHexadecimal(string) }
+        
+        var binaryString = String(int, radix: 2)
+        binaryString.pad(with: "0", untilDivisibleBy: 4)
+        
+        let versionBinary = binaryString.poppingFirst(3)
+        let versionInt = Int(versionBinary, radix: 2)!
+        
+        let typeIDBinary = binaryString.poppingFirst(3)
+        let typeIDInt = Int(typeIDBinary, radix: 2)!
+        
+        if typeIDInt == 4 {
+            let (packet, _) = LiteralPacket.factory(version: versionInt, remainingString: binaryString)
+            self = .literal(packet)
+//            self = .literal(LiteralPacket(version: versionInt, remainingString: binaryString))
+        } else {
+            let (packet, _) = OperatorPacket.factory(version: versionInt, typeID: typeIDInt, remainingString: binaryString)
+            self = .operator(packet)
+//            self = .operator(OperatorPacket(version: versionInt, typeID: typeIDInt, subPackets: []))
+        }
+    }
+    
+    /// A way to provide an input string, then return the remaining string to create other.
+    static func factory(from binaryString: String) -> (Packet, String) {
+        var binaryString = binaryString
+        
+        let versionBinary = binaryString.poppingFirst(3)
+        let versionInt = Int(versionBinary, radix: 2)!
+        
+        let typeIDBinary = binaryString.poppingFirst(3)
+        let typeIDInt = Int(typeIDBinary, radix: 2)!
+        
+        if typeIDInt == 4 {
+            let (newPacket, remainingString) = LiteralPacket.factory(version: versionInt, remainingString: binaryString)
+            return (.literal(newPacket), remainingString)
+        } else {
+            let (newPacket, remainingString) = OperatorPacket.factory(version: versionInt, typeID: typeIDInt, remainingString: binaryString)
+            return (.operator(newPacket), remainingString)
+        }
+    }
+}
+
+struct LiteralPacket {
     let version: Int
-    let typeID: Int // Might not need this because the packet type ID for a literal packet will always be 4
     let literalValue: Int
     
     init(string: String) throws {
@@ -65,8 +110,7 @@ struct LiteralPacket {
         let versionBinary = binaryString.poppingFirst(3)
         let versionInt = Int(versionBinary, radix: 2)!
 
-        let typeIDBinary = binaryString.poppingFirst(3)
-        let typeIDInt = Int(typeIDBinary, radix: 2)!
+        _ = binaryString.poppingFirst(3) // Pop off TypeID
         
         var literalValueString = ""
         
@@ -83,14 +127,63 @@ struct LiteralPacket {
         let literalValueInt = Int(literalValueString, radix: 2)!
         
         version = versionInt
-        typeID = typeIDInt
         literalValue = literalValueInt
+    }
+    
+    init(version: Int, remainingString: String) {
+        var binaryString = remainingString
+        
+        var literalValueString = ""
+        
+        var continueDigit = "1"
+        
+        while continueDigit == "1" {
+            var fiveBits = binaryString.poppingFirst(5)
+            
+            continueDigit = fiveBits.poppingFirst(1)
+            
+            literalValueString += fiveBits
+        }
+        
+        let literalValueInt = Int(literalValueString, radix: 2)!
+        
+        self.version = version
+        self.literalValue = literalValueInt
+    }
+    
+    init(version: Int, literalValue: Int) {
+        self.version = version
+        self.literalValue = literalValue
+    }
+    
+    static func factory(version: Int, remainingString binaryString: String) -> (LiteralPacket, String) {
+        var binaryString = binaryString
+        
+        var literalValueString = ""
+        
+        var continueDigit = "1"
+        
+        while continueDigit == "1" {
+            var fiveBits = binaryString.poppingFirst(5)
+            
+            continueDigit = fiveBits.poppingFirst(1)
+            
+            literalValueString += fiveBits
+        }
+        
+        let literalValueInt = Int(literalValueString, radix: 2)!
+        
+        let newPacket = LiteralPacket(version: version, literalValue: literalValueInt)
+        
+        return (newPacket, binaryString)
     }
 }
 
 do {
     let test = try LiteralPacket(string: "D2FE28")
     print(test)
+    let test2 = try Packet(string: "D2FE28")
+    print(test2)
 } catch {
     error
 }
@@ -98,8 +191,7 @@ do {
 struct OperatorPacket {
     let version: Int
     let typeID: Int
-    let lengthTypeID: String
-    let subpacketLength: Int
+    let subPackets: [Packet]
     
     init(string: String) throws {
         guard let int = Int(string, radix: 16) else { throw PacketError.stringIsNotValidHexadecimal(string) }
@@ -114,21 +206,68 @@ struct OperatorPacket {
         let typeIDInt = Int(typeIDBinary, radix: 2)!
         
         let lengthTypeIDBinary = binaryString.poppingFirst(1)
-        let lengthLength = lengthTypeIDBinary == "0" ? 15 : 11
+        let lengthLength = lengthTypeIDBinary == "0" ? 15 : 11 // FIXME: If "0", next 15 bits will provide the length of subpackets. If "1", next 11 bits will provide the number of subpackets.
         
         let subPacketLengthBinary = binaryString.poppingFirst(lengthLength)
         let subPacketLengthInt = Int(subPacketLengthBinary, radix: 2)!
         
         version = versionInt
         typeID = typeIDInt
-        lengthTypeID = lengthTypeIDBinary
-        subpacketLength = subPacketLengthInt
+        subPackets = []
+    }
+    
+    init(version: Int, typeID: Int, subPackets: [Packet]) {
+        self.version = version
+        self.typeID = typeID
+        self.subPackets = subPackets
+    }
+    
+    static func factory(version: Int, typeID: Int, remainingString binaryString: String) -> (OperatorPacket, String) {
+        var binaryString = binaryString
+        
+        let lengthTypeID = binaryString.poppingFirst(1)
+        
+        var subPackets = [Packet]()
+        
+        if lengthTypeID == "0" {
+            // Next 15 bits are a number that represents the total length in bits of the sub-packets contained by this packet.
+            let numberBinary = binaryString.poppingFirst(15)
+            let number = Int(numberBinary, radix: 2)!
+            
+            let startingCount = binaryString.count
+            var subtractedCharacters = 0
+            
+            while subtractedCharacters < number {
+                let (nextSubPacket, remainingString) = Packet.factory(from: binaryString)
+                
+                subPackets += [nextSubPacket]
+                
+                subtractedCharacters = startingCount - remainingString.count
+            }
+        } else {
+            // Next 11 bits are a number that represents the number of sub-packets immediately contained by this packet.
+            let numberBinary = binaryString.poppingFirst(11)
+            let number = Int(numberBinary, radix: 2)!
+            
+            while subPackets.count < number {
+                let (nextSubPacket, remainingString) = Packet.factory(from: binaryString)
+                
+                subPackets += [nextSubPacket]
+                binaryString = remainingString
+            }
+        }
+        
+        let newPacket = OperatorPacket(version: version, typeID: typeID, subPackets: subPackets)
+        
+        return (newPacket, binaryString)
     }
 }
 
 do {
     let test = try OperatorPacket(string: "38006F45291200")
     print(test)
+    let test2 = try Packet(string: "38006F45291200")
+    print(test2)
 } catch {
     error
 }
