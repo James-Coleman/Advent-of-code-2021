434918784362747bbeee020983d1146797787415
James Coleman <mrjameslcoleman@gmail.com>
2021-12-18 14:27:16 +0000

    Added a split function for Day 18

--- Advent of Code 2021/Day 18 - Snailfish.playground/Contents.swift
+++ Advent of Code 2021/Day 18 - Snailfish.playground/Contents.swift
@@ -33,7 +33,7 @@ class SnailFishNumberWrapper {
         }
     }
     
-    let number: SnailFishNumber
+    var number: SnailFishNumber
     
     init(throws any: Any) throws {
         self.number = try SnailFishNumber(throws: any)
@@ -57,8 +57,57 @@ class SnailFishNumberWrapper {
         }
     }
     
+    func flattened(level: Int = 0) -> [(level: Int, snailFishNumber: SnailFishNumberWrapper)] {
+        switch number {
+            case .integer:
+                return [(level, self)]
+            case let .pair(left, right):
+                return [left.flattened(level: level + 1), right.flattened(level: level + 1)].flatMap { $0 }
+        }
+    }
+    
     static func + (lhs: SnailFishNumberWrapper, rhs: SnailFishNumberWrapper) -> SnailFishNumberWrapper {
         SnailFishNumberWrapper(pair: (lhs, rhs))
+    }
+    
+    /**
+     - returns: Bool of if something was split
+     */
+    static func splitIfNecessary(_ number: SnailFishNumberWrapper) -> Bool {
+        let flattened = number.flattened
+        
+        for wrapper in flattened {
+            guard case let .integer(int) = wrapper.number else { continue }
+            
+            if int >= 10 {
+                let (quotient, remainder) = int.quotientAndRemainder(dividingBy: 2)
+                
+                guard let left = SnailFishNumberWrapper(quotient), let right = SnailFishNumberWrapper(quotient + remainder) else { continue }
+                
+                wrapper.number = .pair(left, right)
+                
+                return true
+            }
+        }
+        
+        return false
+    }
+    
+    /**
+     - returns: Bool of if something was split
+     */
+    static func explodeIfNecessary(_ number: SnailFishNumberWrapper) -> Bool {
+        let flattened = number.flattened()
+        
+        for (index, wrapper) in flattened.enumerated() {
+            if wrapper.level >= 4 {
+                
+                
+                return true
+            }
+        }
+        
+        return false
     }
 }
 
@@ -79,8 +128,23 @@ let example2 = SnailFishNumberWrapper([[1,2],3])
 let example3 = SnailFishNumberWrapper([9,[8,7]])
 let example4 = SnailFishNumberWrapper([[1,9], [8,5]])
 example4?.flattened
+example4?.flattened()
 let example5 = SnailFishNumberWrapper([[[[1,2],[3,4]],[[5,6],[7,8]]],9])
 let example6 = SnailFishNumberWrapper([[[9,[3,8]],[[0,9],6]],[[[3,7],[4,9]],3]])
 let example7 = SnailFishNumberWrapper([[[[1,3],[5,3]],[[1,3],[8,7]]],[[[4,9],[6,9]],[[8,2],[7,3]]]])
 
 SnailFishNumberWrapper([1,2])! + SnailFishNumberWrapper([[3,4],5])!
+
+let splitExample0 = SnailFishNumberWrapper(9)!
+let splitExample1 = SnailFishNumberWrapper(10)!
+let splitExample2 = SnailFishNumberWrapper(11)!
+let splitExample3 = SnailFishNumberWrapper(12)!
+SnailFishNumberWrapper.splitIfNecessary(splitExample0)
+SnailFishNumberWrapper.splitIfNecessary(splitExample1)
+SnailFishNumberWrapper.splitIfNecessary(splitExample2)
+SnailFishNumberWrapper.splitIfNecessary(splitExample3)
+splitExample0
+splitExample1
+splitExample2
+splitExample3
+
