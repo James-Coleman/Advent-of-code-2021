67c7b8b4582beb08f4aaf0f618e862317343e185
James Coleman <mrjameslcoleman@gmail.com>
2021-12-18 09:03:04 +0000

    Removed the non wrapped SnailFishNumber

--- Advent of Code 2021/Day 18 - Snailfish.playground/Contents.swift
+++ Advent of Code 2021/Day 18 - Snailfish.playground/Contents.swift
@@ -6,39 +6,6 @@ enum SnailFishNumberError: Error {
     case couldntInit(from: Any)
 }
 
-indirect enum SnailFishNumber {
-    case integer(Int)
-    case pair(SnailFishNumber, SnailFishNumber)
-    
-    init(_ any: Any) throws {
-        if let int = any as? Int {
-            self = .integer(int)
-        } else if let array = any as? [Any] {
-            self = .pair(try SnailFishNumber(array[0]), try SnailFishNumber(array[1]))
-        } else {
-            throw SnailFishNumberError.couldntInit(from: any)
-        }
-    }
-}
-
-extension SnailFishNumber: CustomStringConvertible {
-    var description: String {
-        switch self {
-            case let .integer(int):
-                return "\(int)"
-            case let .pair(left, right):
-                return "[\(left),\(right)]"
-        }
-    }
-}
-
-// This is now only being used for example3
-extension SnailFishNumber: ExpressibleByIntegerLiteral {
-    init(integerLiteral value: IntegerLiteralType) {
-        self = .integer(value)
-    }
-}
-
 class SnailFishNumberWrapper {
     enum SnailFishNumber {
         case integer(Int)
@@ -82,42 +49,15 @@ extension SnailFishNumberWrapper: CustomStringConvertible {
     }
 }
 
-//extension SnailFishNumber: ExpressibleByArrayLiteral {
-//    typealias ArrayLiteralElement = Any
-//
-//    init(arrayLiteral elements: ArrayLiteralElement...) {
-////        if let firstNumber = elements[0] as? Int, let secondNumber = elements[1] as? Int {
-////            self = .pair(.integer(firstNumber), .integer(secondNumber))
-////        } else if let firstNumber = elements[0] as? Int, let right = elements[1] as? [Any] {
-////            self = .pair(.integer(firstNumber), try SnailFishNumber(elements[1]))// .pair(SnailFishNumber(right[0]), SnailFishNumber(right[1])))
-////        } else if let secondNumber = elements[1] as? Int {
-////            self = .pair(try SnailFishNumber(elements[0]), .integer(secondNumber))
-////        }
-//
-//        if let elements = elements as? [Int] {
-//            self = .pair(.integer(elements[0]), .integer(elements[1]))
-//        } else {
-//            self = SnailFishNumber(elements)
-//        }
-//    }
-//}
-
-let example1 = SnailFishNumber.pair(.integer(1), .integer(2))
-let example2 = SnailFishNumber.pair(.pair(.integer(1), .integer(2)), .integer(3))
-let example3 = SnailFishNumber.pair(9, .pair(8, 7))
-
 do {
-    let example4 = try SnailFishNumber([[1,9], [8,5]])
-    let example5 = try SnailFishNumber([[[[1,2],[3,4]],[[5,6],[7,8]]],9])
-    let example6 = try SnailFishNumber([[[9,[3,8]],[[0,9],6]],[[[3,7],[4,9]],3]])
-    let example7 = try SnailFishNumber([[[[1,3],[5,3]],[[1,3],[8,7]]],[[[4,9],[6,9]],[[8,2],[7,3]]]])
-} catch {
-    error
-}
-
-do {
+    let example1 = try SnailFishNumberWrapper([1,2])
+    let example2 = try SnailFishNumberWrapper([[1,2],3])
+    let example3 = try SnailFishNumberWrapper([9,[8,7]])
     let example4 = try SnailFishNumberWrapper([[1,9], [8,5]])
     example4.flattened
+    let example5 = try SnailFishNumberWrapper([[[[1,2],[3,4]],[[5,6],[7,8]]],9])
+    let example6 = try SnailFishNumberWrapper([[[9,[3,8]],[[0,9],6]],[[[3,7],[4,9]],3]])
+    let example7 = try SnailFishNumberWrapper([[[[1,3],[5,3]],[[1,3],[8,7]]],[[[4,9],[6,9]],[[8,2],[7,3]]]])
 } catch {
     
 }
