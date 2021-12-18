b42091de334dc3771ecb617889fd2af040213994
James Coleman <mrjameslcoleman@gmail.com>
2021-12-18 11:58:14 +0000

    Added optional initialisers and adding function

--- Advent of Code 2021/Day 18 - Snailfish.playground/Contents.swift
+++ Advent of Code 2021/Day 18 - Snailfish.playground/Contents.swift
@@ -11,21 +11,41 @@ class SnailFishNumberWrapper {
         case integer(Int)
         case pair(SnailFishNumberWrapper, SnailFishNumberWrapper)
         
-        init(_ any: Any) throws {
+        init(throws any: Any) throws {
             if let int = any as? Int {
                 self = .integer(int)
             } else if let array = any as? [Any] {
-                self = .pair(try SnailFishNumberWrapper(array[0]), try SnailFishNumberWrapper(array[1]))
+                self = .pair(try SnailFishNumberWrapper(throws: array[0]), try SnailFishNumberWrapper(throws: array[1]))
             } else {
                 throw SnailFishNumberError.couldntInit(from: any)
+            }
+        }
+        
+        init?(_ any: Any) {
+            if let int = any as? Int {
+                self = .integer(int)
+            } else if let array = any as? [Any] {
+                guard let left = SnailFishNumberWrapper(array[0]), let right = SnailFishNumberWrapper(array[1]) else { return nil }
+                self = .pair(left, right)
+            } else {
+                return nil
             }
         }
     }
     
     let number: SnailFishNumber
     
-    init(_ any: Any) throws {
-        self.number = try SnailFishNumber(any)
+    init(throws any: Any) throws {
+        self.number = try SnailFishNumber(throws: any)
+    }
+    
+    init?(_ any: Any) {
+        guard let number = SnailFishNumber(any) else { return nil }
+        self.number = number
+    }
+    
+    init(pair: (SnailFishNumberWrapper, SnailFishNumberWrapper)) {
+        self.number = .pair(pair.0, pair.1)
     }
     
     var flattened: [SnailFishNumberWrapper] {
@@ -35,6 +55,10 @@ class SnailFishNumberWrapper {
             case let .pair(left, right):
                 return [left.flattened, right.flattened].flatMap { $0 }
         }
+    }
+    
+    static func + (lhs: SnailFishNumberWrapper, rhs: SnailFishNumberWrapper) -> SnailFishNumberWrapper {
+        SnailFishNumberWrapper(pair: (lhs, rhs))
     }
 }
 
@@ -49,15 +73,14 @@ extension SnailFishNumberWrapper: CustomStringConvertible {
     }
 }
 
-do {
-    let example1 = try SnailFishNumberWrapper([1,2])
-    let example2 = try SnailFishNumberWrapper([[1,2],3])
-    let example3 = try SnailFishNumberWrapper([9,[8,7]])
-    let example4 = try SnailFishNumberWrapper([[1,9], [8,5]])
-    example4.flattened
-    let example5 = try SnailFishNumberWrapper([[[[1,2],[3,4]],[[5,6],[7,8]]],9])
-    let example6 = try SnailFishNumberWrapper([[[9,[3,8]],[[0,9],6]],[[[3,7],[4,9]],3]])
-    let example7 = try SnailFishNumberWrapper([[[[1,3],[5,3]],[[1,3],[8,7]]],[[[4,9],[6,9]],[[8,2],[7,3]]]])
-} catch {
-    
-}
+
+let example1 = SnailFishNumberWrapper([1,2])
+let example2 = SnailFishNumberWrapper([[1,2],3])
+let example3 = SnailFishNumberWrapper([9,[8,7]])
+let example4 = SnailFishNumberWrapper([[1,9], [8,5]])
+example4?.flattened
+let example5 = SnailFishNumberWrapper([[[[1,2],[3,4]],[[5,6],[7,8]]],9])
+let example6 = SnailFishNumberWrapper([[[9,[3,8]],[[0,9],6]],[[[3,7],[4,9]],3]])
+let example7 = SnailFishNumberWrapper([[[[1,3],[5,3]],[[1,3],[8,7]]],[[[4,9],[6,9]],[[8,2],[7,3]]]])
+
+SnailFishNumberWrapper([1,2])! + SnailFishNumberWrapper([[3,4],5])!
