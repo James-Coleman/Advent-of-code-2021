37451ef8a23868ed08d8882bc4c0d069f72632c9
James Coleman <mrjameslcoleman@gmail.com>
2021-12-19 20:59:51 +0000

    Completed Day 18 Part 2

--- Advent of Code 2021/Day 18 - Snailfish.playground/Pages/Explosions First.xcplaygroundpage/Contents.swift
+++ Advent of Code 2021/Day 18 - Snailfish.playground/Pages/Explosions First.xcplaygroundpage/Contents.swift
@@ -388,3 +388,41 @@ let exampleHomework = SnailFishNumber([[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,
 
 exampleHomework.magnitude
  */
+
+/*
+let exampleInput: [Any] = [
+    [[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]],
+    [[[5,[2,8]],4],[5,[[9,9],0]]],
+    [6,[[[6,2],[5,6]],[[7,6],[4,7]]]],
+    [[[6,[0,7]],[0,9]],[4,[9,[9,0]]]],
+    [[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]],
+    [[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]],
+    [[[[5,4],[7,7]],8],[[8,3],8]],
+    [[9,3],[[9,9],[6,[4,9]]]],
+    [[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]],
+    [[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]
+]
+
+var largestMagnitude = 0
+
+for (firstIndex, firstAny) in exampleInput.enumerated() {
+    for (secondIndex, secondAny) in exampleInput.enumerated() {
+        guard firstIndex != secondIndex else { continue }
+        
+        guard
+            let firstNumber = SnailFishNumber(firstAny),
+            let secondNumber = SnailFishNumber(secondAny)
+        else { continue }
+        
+        let added = firstNumber + secondNumber
+        
+        let addedMagnitude = added.magnitude
+        
+        if addedMagnitude > largestMagnitude {
+            largestMagnitude = addedMagnitude
+        }
+    }
+}
+
+largestMagnitude // 3993 (correct)
+*/
